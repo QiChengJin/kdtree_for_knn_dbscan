982a1a049969bfa0e1326bf8a93ab034b98a073a
QiChengJin <qjin5@illinois.edu>
2024-03-16 11:25:18 -0500

    Committing changes before pulling

---
+++ entry/main.cpp
@@ -0,0 +1,15 @@
+//project
+
+#include <iostream>
+#include <vector>
+#include "kdtree.h"
+
+int main() {
+  std::vector<int> v;
+  for (int i = 0; i < 5; ++i) v.push_back(i);
+  std::cout << v[4] << std::endl;
+  // std::cout << "hello" << std::endl;
+  return 0;
+}
+
+
